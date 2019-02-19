dccd3e5688c4c9f24f4f032c37d9cd38069d6e16
Kelly <kellyliang95@gmail.com>
2019-02-19 11:37:42 -0500

    pins queries completed

--- backend/db/queries/pinsQueries.js
+++ backend/db/queries/pinsQueries.js
@@ -1,11 +1,13 @@
-const { db } = require('./index.js')
+const {
+  db
+} = require('./index.js')
 
 const getAllPins = (req, res, next) => {
   db.any("SELECT * FROM pins")
     .then(data => {
       res.status(200).json({
         status: "Success",
-        data: data, 
+        data: data,
         message: "Received all pins"
       });
     })
@@ -31,11 +33,11 @@ const getSinglePin = (req, res, next) => {
 
 const createPin = (req, res, next) => {
   db
-  .none(
-    "INSERT INTO pins(users_id, boards_id, description, image_url) VALUES(${users_id}, ${boards_id}, ${description}, ${image_url})",
-  req.body
-  )
-    .then(()=> {
+    .none(
+      "INSERT INTO pins(users_id, boards_id, description, image_url) VALUES(${users_id}, ${boards_id}, ${description}, ${image_url})",
+      req.body
+    )
+    .then(() => {
       res.status(200).json({
         status: "Success",
         message: "Created a pin"
@@ -46,8 +48,45 @@ const createPin = (req, res, next) => {
     });
 };
 
+const editPin = (req, res, next) => {
+  db.none("UPDATE pins SET users_id=${users_id}, boards_id=${boards_id}, description=${description}, image_url=${image_url} WHERE id=${id}", 
+  {
+    users_id: parseInt(req.body.users_id),
+    boards_id: parseInt(req.body.boards_id),
+    description: req.body.description,
+    image_url: req.body.image_url,
+    id: parseInt(req.params.id)
+    }
+  )
+  .then(() => {
+    res.status(200).json({
+      status: "success",
+      message: "Updated a pin"
+    });
+  })
+  .catch(err => {
+    return next(err);
+  });
+};
+
+const deletePin = (req, res, next) => {
+  let pinId = parseInt(req.params.id);
+  db
+    .result("DELETE FROM pins WHERE id=$1", pinId)
+    .then(result => {
+      res.status(200).json({
+        status: "success",
+        message: "Removed pin",
+        result: result
+      });
+    })
+    .catch(err => next(err));
+};
+
 module.exports = {
   getAllPins,
   getSinglePin,
-  createPin
+  createPin,
+  editPin,
+  deletePin
 };
--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllPins, getSinglePin, createPin } = require("../db/queries/pinsQueries");
+const { getAllPins, getSinglePin, createPin, editPin, deletePin, getPinsInBoard } = require("../db/queries/pinsQueries");
 
 router.get("/", getAllPins);
 router.get("/:id", getSinglePin);
 router.post("/", createPin);
+router.put("/:id", editPin);
+router.delete("/:id", deletePin);
 
 module.exports = router; 
